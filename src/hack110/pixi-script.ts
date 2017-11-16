28a92774fe19d7f0ea3901806ecaa0317fb33be0
Hank Henry Hester <hankh@live.unc.edu>
2017-11-16 11:40:03 -0500

    added images

--- src/hack110/pixi-script.ts
+++ src/hack110/pixi-script.ts
@@ -12,35 +12,38 @@ const app: Application = new Application(512, 512);
 document.body.appendChild(app.view);
 
 class Blob {
-  graphics: Graphics;
+  sprite: Sprite;
   direction: number = 1;
-  constructor(graphics: Graphics) {
-    this.graphics = graphics;
+  constructor(sprite: Sprite) {
+    this.sprite = sprite;
   }
 }
 
-const jiffrey: Graphics = new Graphics();
-jiffrey.beginFill(0xaa0000);
-jiffrey.drawRect(0, 0, 20, 40);
-jiffrey.x = 30;
+let background: Sprite = Sprite.fromImage("./dungeon.png");
+app.stage.addChild(background);
+
+let jiffrey: Sprite = Sprite.fromImage("./jiffrey.jpg");
+jiffrey.scale.x = 0.12;
+jiffrey.scale.y = 0.12;
+jiffrey.x = 40;
 jiffrey.y = 240;
 app.stage.addChild(jiffrey);
 
-const cpuhat: Graphics = new Graphics();
-cpuhat.beginFill(0x4b9cd3);
-cpuhat.drawCircle(490, 256, 10);
+let cpuhat: Sprite = Sprite.fromImage("./cpuhat.png");
+cpuhat.scale.x = 0.1;
+cpuhat.scale.y = 0.1;
+cpuhat.x = 450;
+cpuhat.y = 240;
 app.stage.addChild(cpuhat);
 
 let blobs: Blob[] = [];
 for (let i: number = 1; i <= 4; i++) {
-  let graphics: Graphics = new Graphics();
-  graphics.beginFill(0xffffff);
-  graphics.drawEllipse(0, 0, 10, 20);
-  graphics.x = 450 / 4 * i - 20;
-  graphics.y = 256;
-  let blob: Blob = new Blob(graphics);
+  let sprite: Sprite = Sprite.fromImage("./blob.png");
+  sprite.x = 450 / 4 * i - 20;
+  sprite.y = 256;
+  let blob: Blob = new Blob(sprite);
   blobs.push(blob);
-  app.stage.addChild(blob.graphics);
+  app.stage.addChild(blob.sprite);
 }
 
 window.onkeydown = function(e: KeyboardEvent): void {
@@ -49,7 +52,6 @@ window.onkeydown = function(e: KeyboardEvent): void {
   const RIGHT: number = 39;
   const DOWN: number = 40;
   const STEP: number = 5;
-  // e.preventDefault();
   if (e.keyCode === LEFT) {
     jiffrey.x -= STEP;
   } else if (e.keyCode === UP) {
@@ -67,10 +69,15 @@ function isColliding(a: DisplayObject, b: DisplayObject): boolean {
   return ab.x + ab.width > bb.x && ab.x < bb.x + bb.width && ab.y + ab.height > bb.y && ab.y < bb.y + bb.height;
 }
 
+function resetJiffrey(): void {
+  jiffrey.x = 30;
+  jiffrey.y = 240;
+}
+
 let hasWon: boolean = false;
 
-const message: Text = new Text("You won!!");
-const messageBox: Graphics = new Graphics();
+let message: Text = new Text("You won!!");
+let messageBox: Graphics = new Graphics();
 
 function handleWin(): void {
   message.x = 216;
@@ -78,7 +85,7 @@ function handleWin(): void {
   message.style.fill = 0xffffff;
   messageBox.beginFill(0x4444aa, 0.4);
   messageBox.drawRect(0, 0, 120, 50);
-  messageBox.x = 256 - 40;
+  messageBox.x = 256 - 45;
   messageBox.y = 256 - 25;
   app.stage.addChild(messageBox);
   app.stage.addChild(message);
@@ -88,27 +95,25 @@ function handleWin(): void {
 app.ticker.add(function(delta: number): void {
   for (let i: number = 0; i < blobs.length; i++) {
     const blob: Blob = blobs[i];
-    blob.graphics.y += 5 * blob.direction;
-    if (Math.random() < 0.01) {
+    blob.sprite.y += 5 * blob.direction;
+    if (Math.random() < 0.01) { // 1% chance every tick
       blob.direction *= -1;
     }
-    if (blob.graphics.y <= 0) {
-      blob.direction *= -1;
-      blob.graphics.y = 1;
-    } else if (blob.graphics.y >= 512) {
-      blob.direction *= -1;
-      blob.graphics.y = 511;
+    if (blob.sprite.y <= 0) {
+      blob.direction = 1;
+      blob.sprite.y = 1;
+    } else if (blob.sprite.y >= 512) {
+      blob.direction = -1;
+      blob.sprite.y = 511;
     }
-    if (isColliding(jiffrey, blob.graphics)) {
-      jiffrey.x = 30;
-      jiffrey.y = 240;
+    if (isColliding(jiffrey, blob.sprite)) {
+      resetJiffrey();
     }
     if (isColliding(jiffrey, cpuhat)) {
       handleWin();
     }
     if (isColliding(jiffrey, messageBox) && hasWon) {
-      jiffrey.x = 30;
-      jiffrey.y = 240;
+      resetJiffrey();
       app.stage.removeChild(message);
       app.stage.removeChild(messageBox);
       hasWon = false;

2ba9f867cc7d267ae91097d8c751034cbfc3ecb7
ShaneSP <shanesp@live.unc.edu>
2017-11-16 22:15:22 -0500

    spacing

--- src/hack110/bg-script.ts
+++ src/hack110/bg-script.ts
@@ -1,20 +1,26 @@
 
-/* This import statement is pulling in the 
-   classes we need from the pixi.js API*/
+/* 
+   This import statement is pulling in the 
+   classes we need from the pixi.js API
+*/
 
 import {
     Sprite,
     Application
 } from "pixi.js";
 
-/* Here we're creating the Application object required by
+/* 
+   Here we're creating the Application object required by
    pixi.js to run our game. We can define the window size of 
-   the app in the Application constructor call.*/
+   the app in the Application constructor call.
+*/
 const app: Application = new Application(512, 512);
 document.body.appendChild(app.view); // <-- this appends the app's view property to the HTML document element
 
-/* We've added an image to the background of our application, 
+/* 
+   We've added an image to the background of our application, 
    using the Sprite class--a Sprite is just a visual element in
-   our app.*/
+   our app.
+*/
 let background: Sprite = Sprite.fromImage("./dungeon.png");
 app.stage.addChild(background);
